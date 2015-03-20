eca84f6e1f04c311266dda5abb38dd56c5a67774
Anna Maria Parkkinen <annamaria.parkkinen@gmail.com>
2015-03-20 20:48:17 +0200

    välicommit

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -5,10 +5,24 @@
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
    	  View::make('home.html');
+
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+
+    public static function etusivu(){
+
+      View::make('suunnitelmat/etusivu.html');
+    }
+
+    public static function viinit(){
+      View::make('suunnitelmat/kaikkiviinit.html');
+    }
+
+    public static function viini_show(){
+      View::make('suunnitelmat/show_wine.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,15 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/etusivu', function() {
+  	HelloWorldController::etusivu();
+  });
+
+  $routes->get('viinit', fuction(){
+    HelloWorldController::viinit();
+  }
+
+  $routes->get('/viinit/1', function(){
+    HelloWorldController::viini_show();
+  });
